1e8636507e252e11c038e4025b7b03b18ad948f8
jobaer-ahmed <ajobaer08@gmail.com>
2022-10-26 23:08:16 +0800

    install cors and send data

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const lists = require('./data/lists.json');
 
 app.get('/', (req, res) => {
     res.send('Skill-Up Api is running!');
 });
+
+app.get('/all-lists', (req, res) => {
+    res.send(lists)
+})
 
 app.listen(port, () => {
     console.log('Skill-up server running on port', port);
